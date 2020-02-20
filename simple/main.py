37d447e3a15bea1d8d40f1486c23ab6e3d9365f0
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2020-02-20 14:34:45 -0300

    Minor

--- simple/main.py
+++ simple/main.py
@@ -2,6 +2,11 @@ from __future__ import absolute_import, print_function
 import numpy as np
 import pyopencl as cl
 
+print('Starting NumPy!')
+a_np = np.random.rand(50000).astype(np.float32)
+b_np = np.random.rand(50000).astype(np.float32)
+c_np = (a_np + b_np)
+
 CODE = """
 __kernel void sum(
     __global const float *a_g, __global const float *b_g, __global float *res_g)
@@ -11,10 +16,30 @@ __kernel void sum(
 }
 """
 
+def execute_kernel(ctx, prg):
+  print('Starting OpenCL queue!') 
+  queue = cl.CommandQueue(ctx)
+
+  mf = cl.mem_flags
+  a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a_np)
+  b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b_np)
+  res_g = cl.Buffer(ctx, mf.WRITE_ONLY, a_np.nbytes)
+
+  print('Running!')
+  prg.sum(queue, a_np.shape, None, a_g, b_g, res_g)
+  res_np = np.empty_like(a_np)
+  cl.enqueue_copy(queue, res_np, res_g)
+
+  # Check on CPU with Numpy:
+  print('Validating!')
+  print(res_np - c_np)
+  print(np.linalg.norm(res_np - c_np))
+  assert np.allclose(res_np, c_np)
+  print('Passed validation!')
+
 def print_device(device):
   context = cl.Context([device])
   program = cl.Program(context, CODE).build()
-  kernel = cl.Kernel(program, "test")
 
   print("Platform: " + platform.name)
   print("Device: " + device.name + " (" + cl.device_type.to_string(device.type) + ")")
@@ -26,8 +51,11 @@ def print_device(device):
   print("\tCompute units: \t\t" + str(device.max_compute_units))
   print("\tMax work-group size: \t" + str(device.max_work_group_size))
   print("\tMax work-item size: \t" + str(device.max_work_item_sizes))
+
+  kernel = cl.Kernel(program, "sum")
   print("\tLockstep unit: \t\t" + str(kernel.get_work_group_info(cl.kernel_work_group_info.PREFERRED_WORK_GROUP_SIZE_MULTIPLE, device)))
   print()
+  execute_kernel(context, program)
 
 def print_devices():
   for platform in cl.get_platforms():
@@ -37,32 +65,6 @@ def print_devices():
 print('Listing available devices!')
 print_devices()
 
-print('Starting NumPy!')
-a_np = np.random.rand(50000).astype(np.float32)
-b_np = np.random.rand(50000).astype(np.float32)
-
-print('Starting OpenCL!')
-ctx = cl.create_some_context()
-queue = cl.CommandQueue(ctx)
-
-print('Compiling Kernel!')
-mf = cl.mem_flags
-a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a_np)
-b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b_np)
-prg = cl.Program(ctx, CODE).build()
-res_g = cl.Buffer(ctx, mf.WRITE_ONLY, a_np.nbytes)
-
-print('Running!')
-prg.sum(queue, a_np.shape, None, a_g, b_g, res_g)
-res_np = np.empty_like(a_np)
-cl.enqueue_copy(queue, res_np, res_g)
-
-# Check on CPU with Numpy:
-print('Validating!')
-print(res_np - (a_np + b_np))
-print(np.linalg.norm(res_np - (a_np + b_np)))
-assert np.allclose(res_np, a_np + b_np)
-print('Passed validation!')
 
 if __name__ == '__main__':
     print('Hi! We are in main!')
