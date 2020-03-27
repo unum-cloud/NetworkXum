7a16488b62519c7ac6dfcde9f1e17a2894c66c19
Ashot Vardanian <ashvardanian@gmail.com>
2020-03-27 17:51:00 -0500

    Fixed attributes retrieval

--- pygraphdb/edge.py
+++ pygraphdb/edge.py
@@ -11,6 +11,7 @@ class Edge(object):
         super().__init__()
         assert isinstance(v_from, int) and isinstance(v_from, int), \
             'Non integer IDs arent supported, use hashes'
+        self._id = None
         self.v_from = v_from
         self.v_to = v_to
         self.weight = weight
@@ -19,15 +20,16 @@ class Edge(object):
     def __repr__(self):
         return f'<EdgeSQL(_id={self._id}, v_from={self.v_from}, v_to={self.v_to}, weight={self.weight})>'
 
-    def __getitem__(self, key):
+    def __contains__(self, key):
         if key == '_id':
-            return self._id
-        elif key == 'v_from':
-            return self.v_from
-        elif key == 'v_to':
-            return self.v_to
-        elif key == 'weight':
-            return self.weight
+            return self._id is not None
+        return key in ['weight', 'v_from', 'v_to']
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def __setitem__(self, key, val):
+        return setattr(self, key, val)
 
     def _update_hash(self):
         self._id = (self.v_from + self.v_to) * \
