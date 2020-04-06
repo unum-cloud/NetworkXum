93a8bc8279b2a6af0f59cc0da2cf463bf49111cd
Ashot Vardanian <ashvardanian@gmail.com>
2020-04-06 15:12:43 -0500

    Fixed DB selection for each dataset

---
+++ bench/exporter.py
@@ -0,0 +1,20 @@
+
+class Exporter(object):
+
+    def __init__(
+        self,
+        benchmarks_path: str,
+        report_path: str,
+    ):
+        pass
+
+    def short_for_dataset(
+        self,
+        dataset_name: str,
+    ):
+        pass
+
+    def long_for_each_operation(
+        self,
+    ):
+        pass
