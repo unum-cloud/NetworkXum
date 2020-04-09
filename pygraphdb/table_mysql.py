1dc17db4f537adbeb5a1223e1b5e99064495d448
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2020-04-09 14:21:16 -0500

    Added MySQL configuration step

--- pygraphdb/table_mysql.py
+++ pygraphdb/table_mysql.py
@@ -1,4 +1,4 @@
-from table_sql import PlainSQL
+from pygraphdb.table_sql import PlainSQL
 
 
 class MySQL(PlainSQL):
@@ -10,9 +10,31 @@ class MySQL(PlainSQL):
     def set_pragmas_on_first_launch(self):
         if self.count_edges() > 0:
             return
-        # https://stackoverflow.com/a/60717467/2766161
+        # https://www.percona.com/blog/2014/01/28/10-mysql-performance-tuning-settings-after-installation/
+        # https://www.monitis.com/blog/101-tips-to-mysql-tuning-and-optimization/
         pragmas = [
+            # To allow direct CSV imports.
+            # https://stackoverflow.com/a/60717467/2766161
             'SET GLOBAL local_infile=1;',
+            # We often flush the temporary table after bulk imports.
+            # https://dev.mysql.com/doc/refman/8.0/en/innodb-parameters.html#sysvar_innodb_file_per_table
+            'SET GLOBAL innodb_file_per_table=1;'
+            # NVME SSDs work well with multi-threaded accesses.
+            # https://dev.mysql.com/doc/refman/8.0/en/innodb-parameters.html#sysvar_innodb_read_io_threads
+            'SET GLOBAL innodb_read_io_threads=16;'
+            # Don't use 0 as node or edge ID, unless prespecified.
+            # https://dev.mysql.com/doc/refman/8.0/en/server-system-variables.html#sysvar_insert_id
+            'SET GLOBAL insert_id=1;'
+            # https://dev.mysql.com/doc/refman/5.7/en/innodb-parameters.html#sysvar_innodb_flush_method
+            'SET GLOBAL innodb_flush_method=O_DIRECT;',
+            # https://dev.mysql.com/doc/refman/8.0/en/server-system-variables.html#sysvar_tmp_table_size
+            'SET GLOBAL tmp_table_size=16777216;',
+            # https://dev.mysql.com/doc/refman/8.0/en/server-system-variables.html#sysvar_max_heap_table_size
+            'SET GLOBAL max_heap_table_size=16777216;',
+
+            # Choosing performance over data integrity.
+            # https://dev.mysql.com/doc/refman/8.0/en/innodb-parameters.html#sysvar_innodb_doublewrite
+            #'SET GLOBAL innodb_doublewrite=1;',
         ]
         for p in pragmas:
             self.session.execute(p)
