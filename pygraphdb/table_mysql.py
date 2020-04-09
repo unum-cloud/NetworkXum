4ef3f600ccd4d47dfe1b0f85faa74370d8565a55
Ashot Vardanian <ashvardanian@gmail.com>
2020-04-09 13:00:26 -0500

    Renamed files and regrouped

---
+++ pygraphdb/table_mysql.py
@@ -0,0 +1,44 @@
+
+class MySQL(PlainSQL):
+
+    def __init__(self, url):
+        PlainSQL.__init__(self, url)
+        self.set_pragmas_on_first_launch()
+
+    def set_pragmas_on_first_launch(self):
+        if self.count_edges() > 0:
+            return
+        # https://stackoverflow.com/a/60717467/2766161
+        pragmas = [
+            'SET GLOBAL local_infile=1;',
+        ]
+        for p in pragmas:
+            self.session.execute(p)
+            self.session.commit()
+
+    # def insert_dump(self, path: str) -> int:
+    #     """
+    #         MySQL keeps producing strange errors using SQL injection method.
+    #         So this implementation reverts back to the basic batch inserts.
+    #     """
+    #     return export_edges_into_graph(path, self)
+
+    def insert_dump_native(self, path: str) -> int:
+        """
+            This method requires the file to be mounted on the same filesystem.
+            Unlike Postgres the connection wrapper doesn't allow channeling data to remote DB.            
+        """
+        cnt = self.count_edges()
+        pattern = '''
+        LOAD DATA LOCAL INFILE  '%s'
+        INTO TABLE %s
+        FIELDS TERMINATED BY ',' 
+        LINES TERMINATED BY '\n'
+        IGNORE 1 ROWS
+        (v_from, v_to, weight);
+        '''
+        task = pattern % (path, EdgeNew.__tablename__)
+        self.session.execute(task)
+        self.session.commit()
+        self.insert_table(EdgeNew.__tablename__)
+        return self.count_edges() - cnt
