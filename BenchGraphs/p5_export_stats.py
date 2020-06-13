826f11cdb552f9238a4dc2fff159d7cb893ad3b3
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2020-06-13 13:57:13 -0500

    Extended benchmarks description

--- BenchGraphs/p5_export_stats.py
+++ BenchGraphs/p5_export_stats.py
@@ -43,8 +43,8 @@ class StatsExporterPerOperation():
             # 'TSLRobin',
         ]
         dataset_names = [
-            'graph-communities',
-            'graph-eachmovie-ratings',
+            # 'graph-communities',
+            # 'graph-eachmovie-ratings',
             'graph-patent-citations',
             'graph-mouse-gene',
             'graph-human-brain',
@@ -52,16 +52,61 @@ class StatsExporterPerOperation():
         dataset_for_comparison = 'graph-human-brain'
 
         # Intro.
-        out.add('# PyWrappedDBs Benchmarks Overview')
+        out.add('# How well can different DBs handle graphs (networks)?')
+        out.add('''
+        At [Unum](https://unum.xyz) we develop a neuro-symbolic AI, which means combining discrete structural representations of data and semi-continuous neural representations.
+        The common misconseeption is that CPU/GPU power is the bottleneck for designing AGI, but we would argue that it's the storage layer.
+
+        * CPU<->RAM bandwidth: ~100 GB/s.
+        * GPU<->VRAM bandwidth: ~1,000 GB/s.
+        * CPU<->GPU bandwidth: ~15 GB/s.
+        * GPU<->GPU bandwidth: ~300 GB/s.
+        * CPU<->SSD bandwidth: ~2 GB/s.
+
+        As we can see, the theoretical throughput between storage (SSD) and CPU is by far the biggest bottleneck.
+        2 GB/s doesn't sound very scary, but the problem is that **most databases can hardly saturate 10%% of that capacity (or 200 MB/s)!**
+        When it comes to random (opposed to sequential) read operations the performance drops further to <1%% of channel capacity.
+        That's why it's crucial for us to store the data in the most capable database!
+        ''')
+
+        out.add('## Setup')
+        out.add('### Databases')
+        out.add('''
+        * Neo4J was designed specifically for graphs storage, but crashes consistently, so it was removed from comparison.
+        * SQLite, MySQL, PostgreSQL and other SQL DBs are the foundations of modern entrprise IT infrastructure.
+        * MongoDB is a new NoSQL database currently values at aound $10 Bln.
+        * GraphDB is our in-house solution.
+        ''')
+        out.add('### Device')
+        out.add_current_device_specs()
+        out.add('### Datasets')
+        out.add('''
+        * [Movie Recommendations Network](http://networkrepository.com/rec-eachmovie.php).
+            * Size: 33 Mb.
+            * Edges: 2,811,716.
+            * Average Degree: 90.
+        * [Patent Citation Network](http://networkrepository.com/cit-patent.php).
+            * Size: 77 Mb.
+            * Edges: 16,518,947.
+            * Average Degree: 8.
+        * [Mouse Gene Regulatory Network](http://networkrepository.com/bio-mouse-gene.php).
+            * Size: 300 Mb.
+            * Edges: 14,506,199.
+            * Average Degree: 670.
+        * [Human Brain Network](http://networkrepository.com/bn-human-Jung2015-M87102575.php).
+            * Size: 4 Gb.
+            * Edges: 87'273'967.
+            * Average Degree: 186.
+        ''')
 
         # Sequential Writes: Import CSV
         out.add('## Sequential Writes: Import CSV (edges/sec)')
         out.add('''
-            Every datascience project starts by importing the data.
-            Let's see how long it will take to load an adjacency list into each DB.
-            But before comparing DBs, let's see what our SSD is capable of by simply parsing the list (2 or 3 column CSV).
-            This will be our baseline for estimating the time required to build the indexes in each DB.
-            ''')
+        Every datascience project starts by importing the data.
+        Let's see how long it will take to load an adjacency list into each DB.
+        But before comparing DBs, let's see what our SSD is capable of by simply parsing the list (2 or 3 column CSV).
+        This will be our baseline for estimating the time required to build the indexes in each DB.
+        ''')
 
         out.add(ins.filtered(
             device_name=device_name,
@@ -72,14 +117,14 @@ class StatsExporterPerOperation():
             cell_content_property='operations_per_second',
             row_names=['Parsing in Python', 'SQLiteMem'],
             col_names=dataset_names,
-        ).add_emoji(dataset_for_comparison))
+        ))
 
         out.add('''
-            Most DBs provide some form functionality for faster bulk imports, but not all of them where used in benchmarks for various reasons.
+        Most DBs provide some form functionality for faster bulk imports, but not all of them where used in benchmarks for various reasons.
 
-            * Neo4J supports CSV imports, but it requires duplicating the imported file and constantly crashes (due to Java heap management issues).
-            * PostgreSQL and MySQL dialects of SQL have special functions for importing CSVs, but their functionality is very limited and performance gains aren't substantial. A better approach is to use unindexed table of incoming edges and later submit it into the main store once the data is absorbed.
-            * MongoDB provides a command line tool, but it wasn't used to limit the number of binary dependencies and simlify configuration.
+        * Neo4J supports CSV imports, but it requires duplicating the imported file and constantly crashes (due to Java heap management issues).
+        * PostgreSQL and MySQL dialects of SQL have special functions for importing CSVs, but their functionality is very limited and performance gains aren't substantial. A better approach is to use unindexed table of incoming edges and later submit it into the main store once the data is absorbed. That's how we implemented it.
+        * MongoDB provides a command line tool, but it wasn't used to limit the number of binary dependencies and simplify configuration.
         ''')
 
         out.add(ins.filtered(
@@ -89,49 +134,70 @@ class StatsExporterPerOperation():
             row_name_property='database',
             col_name_property='dataset',
             cell_content_property='operations_per_second',
-            row_names=[*dbs_pygraph, 'Neo4J', *dbs_unum],
+            row_names=[*dbs_pygraph, *dbs_unum],
             col_names=dataset_names,
         ).add_emoji(dataset_for_comparison))
 
         # Read Queries.
         out.add('## Read Queries')
         out.add('''
-            Following are simple lookup operations.
-            Their speed translates into the execution time of analytical queries like:
+        Following are simple lookup operations.
+        Their speed translates into the execution time of analytical queries like:
 
-            * Shortest Path Calculation,
-            * Clustering Analysis,
-            * Pattern Matching.
+        * Shortest Path Calculation,
+        * Clustering Analysis,
+        * Pattern Matching.
 
-            As we are running on a local machine and within the same filesystem,
-            the networking bandwidth and latency between server and client applications
-            can't be a bottleneck.
+        As we are running on a local machine and within the same filesystem,
+        the networking bandwidth and latency between server and client applications
+        can't be a bottleneck.
         ''')
 
         read_ops = [
             ('Random Reads: Find Any Relation',
-             'Given a pair of nodes - find any edge that connects them.'),
+             '''
+             Input: 2 vertex identifiers.
+             Output: edge that connects them.
+             Metric: number of such edges returned per second.
+             '''),
             ('Random Reads: Find Directed Edge',
-             'Given nodes A and B - find any directed edge that goes from A to B.'),
+             '''
+             Input: 2 vertex identifiers (order is important).
+             Output: edge that connects them in given direction.
+             Metric: number of such edges returned per second.
+             '''),
             ('Random Reads: Find Connected Edges',
-             'Find all edges that contain a specific node in any role.'),
+             '''
+             Input: 1 vertex identifier.
+             Output: all edges attached to it.
+             Metric: number of such edges returned per second.
+             '''),
             ('Random Reads: Find Ingoing Edges',
-             'Find all directed edges that end in a specific node.'),
+             '''
+             Input: 1 vertex identifier.
+             Output: all edges incoming into it.
+             Metric: number of such edges returned per second.
+             '''),
             ('Random Reads: Find Friends',
-             'Get IDs of all nodes that share an edge with a given node.'),
+             '''
+             Input: 1 vertex identifier.
+             Output: the identifiers of all unique vertexes that share an edge with the input.
+             Metric: number of neighbor identiefiers returned per second.
+             '''),
             ('Random Reads: Count Friends',
-             'Count the number of edges containing a specific node and their total weight.'),
+             '''
+             Input: 1 vertex identifier.
+             Output: the total number of attached edges and their accumulated weight.
+             Metric: number queries per second.
+             '''),
             ('Random Reads: Count Followers',
-             'Count the number of edges ending in a specific node and their total weight.'),
-
-            # These are essentially the same.
-            # ('Random Reads: Find Outgoing Edges',
-            #  'Find all directed edges that start in a specific node.'),
-            # ('Random Reads: Find Friends of Friends',
-            #  'Get IDs of all nodes that share an edge with neighbors of a given node.'),
-            # ('Random Reads: Count Following',
-            #  'Count the number of edges starting in a specific node and their total weight.'),
+             '''
+             Input: 1 vertex identifier.
+             Output: the total number of incoming edges and their accumulated weight.
+             Metric: number queries per second.
+             ''')
         ]
+
         for read_op, description in read_ops:
 
             out.add(f'### {read_op}')
@@ -152,21 +218,43 @@ class StatsExporterPerOperation():
         out.add('''
         We don't benchmark edge insertions as those operations are uncommon in graph workloads.
         Instead of that we benchmark **upserts** = inserts or updates.
-        Batch operations have different sizes for different DBs depending on memory consumption 
+        Batch operations have different sizes for different DBs depending on memory consumption
         and other limitations of each DB.
         Concurrency is tested only in systems that explicitly support it.
         ''')
 
         write_ops = [
-            'Random Writes: Upsert Edge',
-            # 'Random Writes: Upsert Edge Concurrent',
-            'Random Writes: Upsert Edges Batch',
-            'Random Writes: Remove Edge',
-            # 'Random Writes: Remove Edge Concurrent',
-            'Random Writes: Remove Edges Batch',
+            ('Random Writes: Upsert Edge',
+             '''
+             Input: 1 new edge.
+             Output: success/failure indicator.
+             Metric: number inserted edges per second.
+             '''),
+
+            ('Random Writes: Upsert Edges Batch',
+             '''
+             Input: 500 new edges.
+             Output: 500 success/failure indicators.
+             Metric: number inserted edges per second.
+             '''),
+
+            ('Random Writes: Remove Edge',
+             '''
+             Input: 1 existing edge.
+             Output: success/failure indicator.
+             Metric: number removed edges per second.
+             '''),
+
+            ('Random Writes: Remove Edges Batch',
+             '''
+             Input: 500 existing edges.
+             Output: 500 success/failure indicators.
+             Metric: number removed edges per second.
+             '''),
         ]
-        for write_op in write_ops:
+        for write_op, description in write_ops:
             out.add(f'### {write_op}')
+            out.add(description)
             out.add(ins.filtered(
                 device_name=device_name,
                 benchmark_name=write_op,
@@ -177,20 +265,6 @@ class StatsExporterPerOperation():
                 row_names=[*dbs_pygraph, *dbs_unum],
                 col_names=dataset_names
             ).add_emoji(dataset_for_comparison))
-
-        # Configuration Details
-        out.add('## Device')
-        cores = psutil.cpu_count(logical=False)
-        threads = psutil.cpu_count(logical=True)
-        frequency = psutil.cpu_freq().min
-        ram_gbs = psutil.virtual_memory().total / (2 ** 30)
-        disk_gbs = psutil.disk_usage('/').total / (2 ** 30)
-        out.add(f'''
-            * CPU: {cores} cores, {threads} threads @ {frequency:.2f}Mhz.
-            * RAM: {ram_gbs:.2f} Gb
-            * Disk: {disk_gbs:.2f} Gb
-            * OS: {platform.system()}
-            ''')
 
         out.print_to(config.report_path)
 
