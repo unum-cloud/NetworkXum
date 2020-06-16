2f36b8246c73453c6912828e2302acf410a95284
Ashot Vardanian <ashvardanian@gmail.com>
2020-06-15 21:07:51 -0500

    Both text search engines pass validation tests

--- PyWrappedDocs/ElasticSearch.py
+++ PyWrappedDocs/ElasticSearch.py
@@ -2,7 +2,7 @@ from elasticsearch import Elasticsearch
 
 from PyWrappedDocs.BaseAPI import BaseAPI
 from PyWrappedGraph.Algorithms import *
-from PyWrappedDocs.File import File
+from PyWrappedDocs.TextFile import TextFile
 
 
 class ElasticSearch(BaseAPI):
@@ -18,73 +18,112 @@ class ElasticSearch(BaseAPI):
     __max_batch_size__ = 1000000
     __in_memory__ = False
 
-    def __init__(self, url='http://localhost:9300', **kwargs):
-        BaseAPI.__init__(self, url, **kwargs)
+    def __init__(self, url='http://localhost:9200', db_name='text', **kwargs):
+        BaseAPI.__init__(self, **kwargs)
         self.elastic = Elasticsearch([url])
-        self.db_name = extract_database_name(url)
+        self.db_name = db_name
         self.create_index()
-            
 
     def create_index(self):
         # https://www.elastic.co/guide/en/elasticsearch/reference/master/indices-exists.html
-        if self.elastic.exists(self.db_name):
+        if self.index_exists():
             return
-            
         properties = dict()
         for field in self.indexed_fields:
-            properties[field] = { 'type': 'text' }
+            properties[field] = {'type': 'text'}
         # https://www.elastic.co/guide/en/elasticsearch/reference/master/indices-create-index.html
-        self.elastic.create(self.db_name, body={
-            'settings' : {
-                'number_of_shards' : 1,
+        # https://sarahleejane.github.io/learning/python/2015/10/14/creating-an-elastic-search-index-with-python.html
+        self.elastic.indices.create(self.db_name, body={
+            'settings': {
+                'number_of_shards': 1,
             },
-            'mappings' : {
+            'mappings': {
                 'properties': properties,
             }
         })
 
-    def cound_docs(self):
-        return self.elastic.count(index=self.db_name)
+    def index_exists(self):
+        return self.elastic.indices.exists(self.db_name)
 
-    def upsert_doc(self, doc):
-        result = self.elastic.index(index=self.db_name, id=doc['_id'], body=doc)
-        return result
+    def commit_all(self):
+        return self.elastic.indices.refresh(self.db_name)
 
-    def remove_doc(self, doc):
-        result = self.elastic.delete(index=self.db_name, id=doc['_id'])
-        return result
+    def remove_all(self):
+        # https://www.elastic.co/guide/en/elasticsearch/reference/master/indices-exists.html
+        if self.index_exists():
+            self.elastic.indices.delete(self.db_name)
+        self.create_index()
 
-    def find_by_id(self, identifier:str):
+    def count_docs(self) -> int:
+        if not self.index_exists():
+            return 0
+        return self.elastic.count(index=self.db_name).pop('count', 0)
+
+    def upsert_doc(self, doc, sync=True) -> bool:
+        # https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-index_.html
+        doc_id = doc.pop('_id', None)
+        if doc_id is None:
+            return False
+        result = self.elastic.index(index=self.db_name, id=doc_id, body=doc)
+        result = result.pop('result', None)
+        if (result == 'created') or (result == 'updated'):
+            if sync:
+                self.commit_all()
+            return True
+        return False
+
+    def remove_doc(self, doc, sync=True) -> bool:
+        doc_id = doc.pop('_id', None)
+        if doc_id is None:
+            return False
+        result = self.elastic.delete(index=self.db_name, id=doc_id)
+        result = result.pop('result', None)
+        if (result == 'deleted'):
+            if sync:
+                self.commit_all()
+            return True
+        return False
+
+    def find_by_id(self, identifier: str):
         return self.elastic.get(index=self.db_name, id=identifier)
 
     def find_with_substring(self, field, query):
         # https://www.elastic.co/guide/en/elasticsearch/reference/6.8/query-dsl-term-query.html
-        return self.elastic.search(index=self.db_name, body={
+        docs = self.elastic.search(index=self.db_name, body={
             'query': {
                 'term': {
                     field: query,
                 }
             }
         })
+        return docs['hits']
 
     def find_with_regex(self, field, query):
         # https://www.elastic.co/guide/en/elasticsearch/reference/6.8/query-dsl-regexp-query.html
         # https://lucene.apache.org/core/4_9_0/core/org/apache/lucene/util/automaton/RegExp.html
-        return self.elastic.search(index=self.db_name, body={
+        docs = self.elastic.search(index=self.db_name, body={
             'query': {
                 'regexp': {
-                    field: query,
-                    'flags': 'INTERVAL',
+                    field: {
+                        'value': query,
+                        'flags': 'INTERVAL',
+                    }
                 }
             }
         })
+        return docs['hits']
 
 
 if __name__ == '__main__':
-    sample_file = 'Datasets/nlp-test/nanoformulations.json'
-    es = ElasticSearch()
-    assert es.cound_docs() == 0
-    assert es.upsert_doc(File(sample_file).content)
-    assert es.cound_docs() == 1
-    es.remove_all()
-    assert es.cound_docs() == 0
+    sample_file = 'Datasets/nlp-test/nanoformulations.txt'
+    db = ElasticSearch(url='http://localhost:9200/',
+                       db_name='nlp-test',
+                       indexed_fields=['plain'])
+    db.remove_all()
+    assert db.count_docs() == 0
+    assert db.upsert_doc(TextFile(sample_file).to_dict())
+    assert db.count_docs() == 1
+    assert db.find_with_substring('plain', 'nanoparticles')
+    assert db.find_with_regex('plain', 'nanoparticles')
+    assert db.remove_doc(TextFile(sample_file).to_dict())
+    assert db.count_docs() == 0
--- PyWrappedDocs/TextFile.py
+++ PyWrappedDocs/TextFile.py
@@ -1,17 +1,19 @@
 import os
 import json
+import copy
 
-class File(object):
 
-    def __init__(self, path:str = ''):
+class TextFile(object):
+
+    def __init__(self, path: str = ''):
         object.__init__(self)
         self.reset(path)
 
-    def reset(self, path:str) -> File:        
+    def reset(self, path: str):
         self.path = path
         self.content = dict()
 
-        if len(path):    
+        if len(path):
             filename_w_ext = os.path.basename(path)
             if path.endswith('.txt'):
                 with open(path, 'r') as plain_file:
@@ -20,6 +22,8 @@ class File(object):
                 with open(path, 'r') as plain_file:
                     self.content = json.load(plain_file)
             self.content['_id'] = filename_w_ext
-        
+
         return self
-            
+
+    def to_dict(self) -> dict:
+        return copy.deepcopy(self.content)
